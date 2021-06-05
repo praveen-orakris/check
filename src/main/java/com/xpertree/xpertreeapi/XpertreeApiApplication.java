04ceee1ba81542194d47364067544c13d340d504
tinkubhanu216 <bhanuprasadthandra@gmail.com>
2021-06-05 18:47:15 +0530

    base project setup

---
+++ src/main/java/com/xpertree/xpertreeapi/XpertreeApiApplication.java
@@ -0,0 +1,13 @@
+package com.xpertree.xpertreeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpertreeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpertreeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpertree/xpertreeapi/XpertreeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpertree.xpertreeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpertreeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
